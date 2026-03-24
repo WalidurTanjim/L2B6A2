5149070ff50b935f0769fcf460403da3e0372893
WalidurTanjim <mohammadtanjim91@gmail.com>
2026-03-24 13:24:36 +0600

    globalErrorHandler created

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+const globalErrorHandler = async(err: any, req: Request, res: Response, next: NextFunction) => {
+     const statusCode = err?.statusCode || 500;
+
+     res.status(statusCode).json({
+          success: false,
+          message: err?.message || "Something went wrong!",
+          ...(process.env.NODE_ENV === "development" && { stack: err?.stack })
+     });
+}
+
+export default globalErrorHandler;
