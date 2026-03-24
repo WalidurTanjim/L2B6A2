f15ff461733e51c4fc6cc147a603e0b7b01d2fb1
WalidurTanjim <mohammadtanjim91@gmail.com>
2026-03-24 13:28:37 +0600

    all modules dir created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import initDB from "./config/db";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 const app = express();
 
 // parser
@@ -11,5 +12,8 @@ initDB();
 app.get('/', (req: Request, res: Response) => {
   res.send('Next Level Web Development');
 })
+
+// globalErrorHandler middleware call
+app.use(globalErrorHandler);
 
 export default app;
