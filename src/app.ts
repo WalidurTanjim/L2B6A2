a436b405e36f6a4bffb6c8d4e9800a398147720b
WalidurTanjim <mohammadtanjim91@gmail.com>
2026-03-28 17:12:45 +0600

    404 not found route created

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,15 @@ app.get('/', (req: Request, res: Response) => {
 // users api route
 app.use("/api/v1/users", usersRouter);
 
+// 404 not found route
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    route: req.path
+  })
+})
+
 // globalErrorHandler middleware call
 app.use(globalErrorHandler);
 
